a4326969e1a35ad935c111113bfecb99d023f2c1
WangZhenyu <847315251@qq.com>
2018-10-26 16:09:31 +0800

    增加类注释

--- src/main/java/top/zywork/configuration/WebMvcConfig.java
+++ src/main/java/top/zywork/configuration/WebMvcConfig.java
@@ -4,6 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * WebMvc配置，增加跨域配置<br/>
+ *
+ * 创建于2018-09-21<br/>
+ *
+ * @author 王振宇
+ * @version 1.0
+ */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
